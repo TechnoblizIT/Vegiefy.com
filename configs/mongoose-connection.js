51d9442ef8a33eb9321debb5c00c5346e031630b
SakshiAmbule-6 <sakshiambule1@gmail.com>
2024-09-30 13:55:59 +0530

    fixed issue in add-product section and added styling to profile page

--- configs/mongoose-connection.js
+++ configs/mongoose-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-const dbconnect= mongoose.connect(process.env.MONGODB_URI).then(function(connection) {
+const dbconnect= mongoose.connect("mongodb+srv://sakshiambule1:sakshi1006@cluster0.k6jxt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(function(connection) {
     console.log('Connected to MongoDB')
 }).catch(function(err){
     console.log('Could not connect to the database. Exiting now...')
