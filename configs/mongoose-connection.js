e486f281e252e1fbf78c4f91e9f7927c5e94e6fc
Praduman kathade <praduman.228@gmail.com>
2025-04-01 11:50:01 +0530

    Database issue fix

--- configs/mongoose-connection.js
+++ configs/mongoose-connection.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 
-const dbconnect = mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  connectTimeoutMS: 30000,  // 30 seconds for initial connection
-  socketTimeoutMS: 30000    // 30 seconds for socket activity
-}).then(function(connection) {
-    console.log('Connected to Database');
-}).catch(function(err) {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit(1);  // Exit if the connection fails
-});
+const dbconnect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            connectTimeoutMS: 30000, // 30 seconds for initial connection
+            socketTimeoutMS: 30000   // 30 seconds for socket activity
+        });
+        console.log('✅ Connected to Database');
+    } catch (err) {
+        console.error('❌ Could not connect to the database. Exiting now...', err);
+        process.exit(1); // Exit the process if the connection fails
+    }
+};
+
+dbconnect();
 
 module.exports = dbconnect;
